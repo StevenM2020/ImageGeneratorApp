033c5ca58d1afdee9bd553141ee27d528a2abd93
Steven <steven.bikerider@gmail.com>
2024-02-22 11:32:35 -0500

    Initial Commit

---
+++ ImageGeneratorApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ImageGeneratorApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
